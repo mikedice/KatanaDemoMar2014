7205fd289a17fc3adca8be521869abf9beae0749
Mike Dice <mikedice417@hotmail.com>
2014-03-13 07:54:02 -0700

    Project populated

---
+++ KatanaDemo/HomeModule.cs
@@ -0,0 +1,26 @@
+﻿using Nancy;
+
+namespace WebApplication2
+{
+    public class HomeModule : NancyModule
+    {
+        /// <summary>
+        /// Home Module constructor
+        /// In Nancy, routes are defined in the constructor of the module.
+        /// A route is defined by httpMethod + pattern + action
+        /// </summary>
+        public HomeModule()
+        {
+            // Define the default route '/'
+            // HTTP Action = GET
+            // URL Pattern = '/'
+            // Action is defined by the lambda expression below.
+            Get["/"] = (a) =>
+            {
+                // create a model for the view and return the view. Home.html is rendered.
+                var model = new { title = "Wow. Interesting syntax Nancy." };
+                return View["home", model];
+            };
+        }
+    }
+}
---
+++ KatanaDemo/Startup.cs
@@ -0,0 +1,47 @@
+﻿// Katana Demo
+// Mike Dice (mikedice417@hotmail.com)
+// Created: March 2014.
+
+using System.Web.Http;
+using Owin;
+
+namespace KatanaDemo
+{
+    // Start up a service using the Nancy framework
+    public class StartupNancy
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+    
+    // Start up a service using Microsoft System.Web MVC framework.
+    public class StartupSystemWeb
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //app.UseNancy();
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                "Default",
+                "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+
+            app.UseWebApi(config);
+        }
+    }
+
+    /// <summary>
+    /// This controller is used when we're demo'ing System.Web.Http hosting
+    /// </summary>
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        public string[] Data()
+        {
+            return new string[] {"This", "Is", "Neat"};
+        }
+
+    }
+}
